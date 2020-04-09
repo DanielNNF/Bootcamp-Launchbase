2fe19f4997a1459973f6f16b7412c07c3fbc6124
DanielNNF <62966905+DanielNNF@users.noreply.github.com>
2020-04-09 07:23:51 -0300

    aulabootcamp
    Exemplos dados na aula do bootcamp

---
+++ index.js
@@ -0,0 +1,37 @@
+//Exemplos dados para introdução à programação web
+
+//Aprendendo a utilização de uma string
+
+const nome = 'Daniel '
+const nome2 = "Nicolas"
+const nome3 = 'Filipe'
+const nome4 = `Nathalia e ${nome}`
+
+//Aprendendo a utilização de um number
+
+const notaAluno01 = 10
+const notaAluno02 = 1.6
+const notaAluno03 = 5.9
+const notaAluno04 = 1.8
+const notaAluno05 = 4.9
+
+const media = (notaAluno01+notaAluno02+notaAluno03+notaAluno04+notaAluno05) / 5
+
+//Exemplos de Condicionais
+
+if (media > 6){
+  console.log(`A media foi de ${media}. Parabéns!!!!`)
+}
+else{
+  console.log('Nota abaixo da média 6. Precisa se esforçar mais!!!!!')
+}
+
+// Operadores relacionados
+
+console.log(4=="4")//true
+console.log(4==="4")//false
+console.log((4!="5"))//true
+console.log(4!=="5")//true
+
+//Operadores logicos e Aritmméticos 
+
